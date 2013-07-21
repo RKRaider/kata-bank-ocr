13e653fb6cc51d4bc5a207bded0f262ba4340858
Anthony Navarre <me@anthonynavarre.com>
2013-07-21 14:31:25 -0600

    Support rigid parsing for all numbers :bowtie:
    - Favored getting it working for now
    - Will need to refactor later to support "guessing" features
    - Probably too rigid about whitespace, but very intent-revealing implementation
    - Added TODO note to remove duplication in the specs

--- lib/ocr/digit_parser.rb
+++ lib/ocr/digit_parser.rb
@@ -9,7 +9,114 @@ module OCR
     end
 
     def parse
-      digit.include?('_') ? 0 : 1
+      digits.key(digit)
+    end
+
+    private
+
+    def digits
+      {
+        0 => zero,
+        1 => one,
+        2 => two,
+        3 => three,
+        4 => four,
+        5 => five,
+        6 => six,
+        7 => seven,
+        8 => eight,
+        9 => nine
+      }
+    end
+
+    def zero
+      <<-DIGIT
+ _ 
+| |
+|_|
+
+      DIGIT
+    end
+
+    def one
+      <<-DIGIT
+  |
+  |
+  |
+
+      DIGIT
+    end
+
+    def two
+      <<-DIGIT
+ _ 
+ _|
+|_ 
+
+      DIGIT
+    end
+
+    def three
+      <<-DIGIT
+ _ 
+ _|
+ _|
+
+      DIGIT
+    end
+
+    def four
+      <<-DIGIT
+   
+|_|
+  |
+
+      DIGIT
+    end
+
+    def five
+      <<-DIGIT
+ _ 
+|_ 
+ _|
+
+      DIGIT
+    end
+
+    def six
+      <<-DIGIT
+ _ 
+|_ 
+|_|
+
+      DIGIT
+    end
+
+    def seven
+      <<-DIGIT
+ _ 
+  |
+  |
+
+      DIGIT
+    end
+
+    def eight
+      <<-DIGIT
+ _ 
+|_|
+|_|
+
+      DIGIT
+    end
+
+    def nine
+      <<-DIGIT
+ _ 
+|_|
+ _|
+
+      DIGIT
     end
 
   end
--- spec/ocr_digit_parser_spec.rb
+++ spec/ocr_digit_parser_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# TODO: Remove all this duplication
 describe OCR::DigitParser do
   zero = <<-DIGIT
  _ 
@@ -15,9 +16,73 @@ describe OCR::DigitParser do
 
   DIGIT
 
+  two = <<-DIGIT
+ _ 
+ _|
+|_ 
+
+  DIGIT
+
+  three = <<-DIGIT
+ _ 
+ _|
+ _|
+
+  DIGIT
+
+  four = <<-DIGIT
+   
+|_|
+  |
+
+  DIGIT
+
+  five = <<-DIGIT
+ _ 
+|_ 
+ _|
+
+  DIGIT
+
+  six = <<-DIGIT
+ _ 
+|_ 
+|_|
+
+  DIGIT
+
+  seven = <<-DIGIT
+ _ 
+  |
+  |
+
+  DIGIT
+
+  eight = <<-DIGIT
+ _ 
+|_|
+|_|
+
+  DIGIT
+
+  nine = <<-DIGIT
+ _ 
+|_|
+ _|
+
+  DIGIT
+
   digits = {
     0 => zero,
-    1 => one
+    1 => one,
+    2 => two,
+    3 => three,
+    4 => four,
+    5 => five,
+    6 => six,
+    7 => seven,
+    8 => eight,
+    9 => nine
   }
 
   digits.each do |expected_integer, digit|
