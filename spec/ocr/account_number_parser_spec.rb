aec19c66dbac22d03b124917c98d298d2b38a6e9
Anthony Navarre <me@anthonynavarre.com>
2013-07-22 10:02:09 -0600

    Add extra test cases

--- spec/ocr/account_number_parser_spec.rb
+++ spec/ocr/account_number_parser_spec.rb
@@ -2,21 +2,80 @@ require 'spec_helper'
 
 describe OCR::AccountNumberParser do
 
-  describe 'parse' do
+  describe '#parse' do
 
-    let(:chars) do
-      <<-CHARS
+    examples = {
+      [0,0,0,0,0,0,0,0,0] => %Q(
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || || |
 |_||_||_||_||_||_||_||_||_|
 
-      CHARS
-    end
+    ),
+    [1,1,1,1,1,1,1,1,1] => %Q(
+                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
 
-    subject { OCR::AccountNumberParser.new(chars) }
+    ),
+    [2,2,2,2,2,2,2,2,2] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
 
-    it 'returns an array representing the digits of the account number' do
-      subject.parse.should == [0,0,0,0,0,0,0,0,0]
+    ),
+    [3,3,3,3,3,3,3,3,3] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+ _| _| _| _| _| _| _| _| _|
+    ),
+    [4,4,4,4,4,4,4,4,4] => %Q(
+                           
+|_||_||_||_||_||_||_||_||_|
+  |  |  |  |  |  |  |  |  |
+    ),
+    [5,5,5,5,5,5,5,5,5] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+ _| _| _| _| _| _| _| _| _|
+
+    ),
+    [6,6,6,6,6,6,6,6,6] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+|_||_||_||_||_||_||_||_||_|
+    ),
+    [7,7,7,7,7,7,7,7,7] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
+    ),
+    [8,8,8,8,8,8,8,8,8] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+|_||_||_||_||_||_||_||_||_|
+    ),
+    [9,9,9,9,9,9,9,9,9] => %Q(
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+ _| _| _| _| _| _| _| _| _|
+    ),
+    [1,2,3,4,5,6,7,8,9] => %Q(
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+    )
+    }
+
+    examples.each do |expected_account_number, example|
+
+      context "For the following input: #{example}" do
+        subject { OCR::AccountNumberParser.new(example) }
+
+        it 'returns an array representing the digits of the account number' do
+          subject.parse.should == expected_account_number
+        end
+      end
+
     end
 
   end
